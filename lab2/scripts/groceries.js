84be14570d96f6df06a023224b983fa00cbdb8e5
unknown <ctrlaltdeleteyou@gmail.com>
2024-01-23 16:26:45 -0500

    added intersection

--- lab2/scripts/groceries.js
+++ lab2/scripts/groceries.js
@@ -103,6 +103,9 @@ function restrictListProducts(prods, restriction) {
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
 			product_names.push(prods[i].name);
 		}
+		else if((restriction == "GFVeg") && (prods[i].glutenFree == true) && (prods[i].vegetarian == true)){
+			product_names.push(prods[i].name);
+		}
 		else if (restriction == "None"){
 			product_names.push(prods[i].name);
 		}
